8fe1771b7cbf6e2d6638b4287d73745a3dddf996
favorsyoon <favorsyoon@gmail.com>
2023-02-11 17:59:09 +0900

    first commit

--- services/productService.js
+++ services/productService.js
@@ -1,26 +1,19 @@
 const ProductRepository = require('../repositories/productRepository');
-const {Product} = require('../models');
-class ProductService{
+const { Product } = require('../models');
+class ProductService {
+  productRepository = new ProductRepository(Product);
+  productCreate = async () => {};
 
-    productRepository = new ProductRepository(Product);
-<<<<<<< HEAD
-    productCreate = async ()=>{
-
-    }
-=======
->>>>>>> f48f6ccda3bf90770f67353ddbfef43ab3c6862b
-
-    // 전체 상품 조회
-    getList = async ()=>{
-        const list = await this.productRepository.getList();
-        return list;
-    }
-    // 상품 디테일 페이지
-    getOne = async (id)=>{
-        const one = await this.productRepository.getOne(id);
-        return one;
-    }
-
+  // 전체 상품 조회
+  getList = async () => {
+    const list = await this.productRepository.getList();
+    return list;
+  };
+  // 상품 디테일 페이지
+  getOne = async (id) => {
+    const one = await this.productRepository.getOne(id);
+    return one;
+  };
 }
 
 module.exports = ProductService;
